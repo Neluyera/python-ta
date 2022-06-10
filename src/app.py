87beb36fd712d3b505d2592e6cf1bae2c7e4da45
Neluyera <107265782+Neluyera@users.noreply.github.com>
2022-06-10 17:35:34 +0100

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -5,6 +5,14 @@ import os
 def prime(s):
     # your code goes here
 
+    s = int(s)
+    if s <= 1:
+        return False
+    for factor in range(2,s):
+        if s % factor == 0:
+            return False
+    return True
+
 def solution(s):
     return prime(s)
 
